97f3cffd6640088a69ce60dba20f4f99dfa498a7
Nattawoot <nattawoot.denduangchai@gmail.com>
2020-07-01 15:41:09 +0700

    JS exercises

---
+++ CodeCamp#6 Homework/5-JavaScript/1-js.js
@@ -0,0 +1,13 @@
+alert("Hello, world!")
+function changeColor() {
+    // alert("Change Color Function...")
+    let tcolor = document.querySelector('#in1').value
+    let bcolor = document.querySelector('#in2').value
+
+    // alert(tcolor)
+    // alert(bcolor)
+    document.querySelector('h2').style.color = tcolor
+    document.querySelector('h2').style.backgroundColor = bcolor
+}
+
+console.log("15" != 15)
